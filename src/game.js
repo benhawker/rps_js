0aafbe837efd65b1c38f5f6bd543d80a9d5c9190
Ben <ben.c.hawker@gmail.com>
2015-08-24 16:34:53 +0100

    First commit RPS js

---
+++ src/game.js
@@ -0,0 +1,58 @@
+function Game() {
+	this.options = ["Rock", "Paper", "Scissors"];
+	this.chosen = {};
+	this.playerOneChoice = "";
+	// this.playerTwoChoice = "";
+	this.playerOneCount = 0;
+	this.playerTwoCount = 0;
+	// perhaps not required as can simply use return.
+	this.result = "";
+};
+
+//for computer selection
+Game.prototype.randomSelection = function() {
+	return _.sample(this.options)
+};
+
+//can be extended with 2nd arg for other players
+Game.prototype.makeChoice = function(choice) {
+	this.playerOneChoice = choice;
+};
+
+
+Game.prototype.play = function(playerOne, playerTwo) {
+	if (playerOne === playerTwo) {
+		return "Draw"
+	}
+};
+
+
+
+
+
+
+
+
+
+// function Player() {
+// }
+// Player.prototype.play = function(song) {
+//   this.currentlyPlayingSong = song;
+//   this.isPlaying = true;
+// };
+
+// Player.prototype.pause = function() {
+//   this.isPlaying = false;
+// };
+
+// Player.prototype.resume = function() {
+//   if (this.isPlaying) {
+//     throw new Error("song is already playing");
+//   }
+
+//   this.isPlaying = true;
+// };
+
+// Player.prototype.makeFavorite = function() {
+//   this.currentlyPlayingSong.persistFavoriteStatus(true);
+// };
